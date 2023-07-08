4252e184309e8b678dcf315fe59ab57c4e71763b
Josep Piles Pérez <josep.piles19@gmail.com>
2023-07-08 13:17:28 +0200

    Primeras configuraciones

---
+++ src/main/java/com/CopeApi/CopeApiSpring/CopeApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.CopeApi.CopeApiSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopeApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopeApiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CopeApi/CopeApiSpring/CopeApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CopeApi.CopeApiSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopeApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
